4cad24f99e7a763bd68549de38438d637c7da789
門屋弘毅 <kamonohashiy@gmail.com>
2020-01-10 22:49:54 +0900

    DoorKeeperのパラメータを変更

--- hitDoorkeeper/handler.py
+++ hitDoorkeeper/handler.py
@@ -15,7 +15,7 @@ def main(event, context):
     PREFECTURES = ['北海道', '青森', '岩手', '宮城', '秋田', '山形', '福島', '茨城', '栃木', '群馬', '埼玉', '千葉', '東京', '神奈川', '新潟', '富山', '石川', '福井', '山梨', '長野', '岐阜', '静岡', '愛知', '三重', '滋賀', '京都', '大阪', '兵庫', '奈良', '和歌山', '鳥取', '島根', '岡山', '広島', '山口', '徳島', '香川', '愛媛', '高知', '福岡', '佐賀', '長崎', '熊本', '大分', '宮崎', '鹿児島', '沖縄']
     PREF_ALPHA = ['hokkaido', 'aomori', 'iwate', 'miyagi', 'akita', 'yamagata', 'fukushima', 'ibaraki', 'tochigi', 'gunma', 'saitama', 'chiba', 'tokyo', 'kanagawa', 'niigata', 'toyama', 'ishikawa', 'fukui', 'yamanashi', 'nagano', 'gifu', 'shizuoka', 'aichi', 'mie', 'shiga', 'kyoto', 'osaka', 'hyogo', 'nara', 'wakayama', 'tottori', 'shimane', 'okayama', 'hiroshima', 'yamaguchi', 'tokushima', 'kagawa', 'ehime', 'kochi', 'fukuoka', 'saga', 'nagasaki', 'kumamoto', 'oita', 'miyazaki', 'kagoshima', 'okinawa']
 
-    URL = "https://api.doorkeeper.jp/events?prefecture=" + PREF_ALPHA[int(number)]
+    URL = "https://api.doorkeeper.jp/events?sort=updated_at&prefecture=" + PREF_ALPHA[int(number)]
 
     response = requests.get(URL)
     ary = response.json()
